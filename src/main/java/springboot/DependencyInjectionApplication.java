b0e3462c816d02280640dd37b2951802dfb52bd6
David Liu <mingchang716@gmail.com>
2017-01-07 22:54:37 -0600

    - spring xml configuration with spring boot - instead of using spring annotation configuration, we update the project to use XML configuration for testing

--- src/main/java/springboot/DependencyInjectionApplication.java
+++ src/main/java/springboot/DependencyInjectionApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ComponentScan("guru.springframework")
+//@ComponentScan("guru.springframework")
+@ImportResource("classpath:/spring/spring-config.xml")
 public class DependencyInjectionApplication {
 
 	public static void main(String[] args) {
